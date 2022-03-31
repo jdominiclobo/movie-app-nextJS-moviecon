04b6afdaa75c1c71039f5b842c5dfbe1243e8233
jdominiclobo <jdominiclobo@gmail.com>
2022-03-31 22:55:00 +0530

    font changes

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lobster+Two:ital@1&display=swap"
+            rel="stylesheet"
+          />
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
